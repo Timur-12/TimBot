d44335ca3548c2aef341681a58067c28015652d0
Timur <timissever@gmail.com>
2021-03-04 21:29:52 +0300

    test

---
+++ bot.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+
+
+const Discord = require('discord.js')
+const client = new Discord.Client({
+    partials: ["MESSAGE"]
+})
+
+const BOT_PREFIX = 'T!'
+const MOD_ME_COMMAND = 'mod-me'
+
+client.on('ready', () => {
+    console.log('Our bot is raedy to go')
+})
+
+client.on('messageDelete', msg => {
+    msg.channel.send(`${msg.author} Mesaj Silme!`);
+})
+
+client.on('message', msg => {
+    if (msg.content == 'Timuru seviyorum') {
+        msg.react('❤')
+    }
+
+    if (msg.content === '${BOT_PREFIX}${MOD_ME_COMMAND}') {
+        modeUser(msg.member)
+    }
+})
+
+function modUser(member) {
+    member.rules.add(816019638609379398)
+}
+
+client.login(process.env.BOT_TOKEN)
